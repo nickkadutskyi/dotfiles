71af3a1f04f1bf8631e6733bd9bd45512171688c
Nick Kadutskyi <nick@kadutskyi.com>
2024-04-14 21:55:17 -0700

    adds git

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -2,14 +2,23 @@
   description = "Nick's flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
     # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    packages."aarch64-darwin".default = 
+      let
+        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+      in
+        pkgs.buildEnv {
+        name = "global-packages";
+        paths = with pkgs; [
+          # general tools
+          git
+          # ... add your tools here
+      ];
+    };
   };
 }
