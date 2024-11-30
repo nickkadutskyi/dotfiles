6b3c19553229fa981fa8d251625c8241109038ae
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-30 15:52:42 -0800

    Fixes

--- .config/alacritty/scripts.sh
+++ .config/alacritty/scripts.sh
@@ -2,11 +2,25 @@
 
 b=$(tput bold)
 n=$(tput sgr0)
+TPGROUP="alacritty"
+TPCONF="$HOME/.config/alacritty/.tmux.conf"
+
+# TODO: ability to kill window and related session in selector
+
+# Create a new alacritty window and attach to a new tmux session
+function new-window() {
+    TMUXEXEC=$(which tmux)
+    alacritty msg create-window -e $TMUXEXEC new -A -t $TPGROUP -s 0 \
+        \; source-file $TPCONF \
+        \; if-shell "[ '#W' = 'zsh' ]" "renamew 0" "neww" \
+        \; rename "#{window_id}" \
+        \; set-env -g TMUXEXEC "$TMUXEXEC"
+}
 
 # If there are any windows with alacritty group and not attached to a client
 # then create a new alacritty window and prompt user to select a tmux window
 function init-select-window() {
-    tmux_format="#{window_id}:#W:#{pane_current_command}:#{?window_active_clients,a,n}:#{session_group}"
+    tmux_format="#{window_id}:#{pane_current_command}:#W:#{?window_active_clients,a,n}:#{session_group}"
     # Gets only windows with alacritty group and not attached to a client
     windows=$(tmux list-windows -F "$tmux_format" | grep ':n:alacritty$')
     if [ ! -z "$windows" ]; then
@@ -22,9 +36,9 @@ function select-window() {
         echo "No windows provided"
         exit 1
     fi
-    sel_win=$(echo $1 | awk -F: '{printf "%-4s (%s) %s\n", $1, $3, $2}' | fzf --height 40% --sort --reverse)
+    sel_win=$(echo $1 | awk -F: '{printf "%-4s (%s) %s\n", $1, $2, $3}' | fzf --height 40% --sort --reverse)
     sel_win_id=$(echo $sel_win | cut -w -f1)
-    ses_name=$sel_win_id
+    ses_name=$sel_win_id # tmux session name is the same as window id
     if [ ! -z "$sel_win_id" ]; then
         tmux attach -d -t "$ses_name" \; select-window -t "$sel_win_id"
     else
@@ -38,6 +52,7 @@ function usage() {
     echo "${b}Commands:${n}"
     echo "  ${b}init-select-window${n}               Init tmux widow selector in a new alacritty window."
     echo "  ${b}select-window${n} [list of windows]  Prompt user to select a tmux window to attach to."
+    echo "  ${b}new-window${n}                       Create a new alacritty window and attach to a new tmux session."
 }
 
 case "$1" in
@@ -46,6 +61,9 @@ case "$1" in
     ;;
 "select-window")
     select-window $2
+    ;;
+"new-window")
+    new-window
     ;;
 *)
     usage $0
