88151514fbb287662f75ae22805837c6c2bf6cfa
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-30 17:56:27 -0800

    Adds ability to kill windows from selector

--- .config/alacritty/scripts.sh
+++ .config/alacritty/scripts.sh
@@ -4,12 +4,16 @@ b=$(tput bold)
 n=$(tput sgr0)
 TMGROUP="alacritty"
 TMCONF="$HOME/.config/alacritty/.tmux.conf"
+TMUX_FORMAT="#{window_id}:#{pane_current_command}:#W:#{?window_active_clients,a,n}:#{session_group}"
+TMEXEC=$(which tmux)
+CMD_KILL='tmux kill-session -t $(echo {1} | cut -w -f1 ); tmux kill-window -t $(echo {1} | cut -w -f1)'
+CMD_FORMAT='awk -F: '\''{printf "%-4s (%s) %s\n", $1, $2, $3}'\'''
+CMD_RELOAD=$TMEXEC' list-windows -F "'$TMUX_FORMAT'" | grep ":n:'$TMGROUP'$" | '$CMD_FORMAT
 
 # TODO: ability to kill window and related session in selector
 
 # Create a new alacritty window and attach to a new tmux session
 function new-window() {
-    TMEXEC=$(which tmux)
     alacritty msg create-window -e $TMEXEC new -A -t $TMGROUP -s 0 \
         \; source-file $TMCONF \
         \; if-shell "[ '#W' = 'zsh' ]" "renamew 0" "neww" \
@@ -19,9 +23,7 @@ function new-window() {
 
 # Reattach to all tmux windows in alacritty group
 function reattach-all() {
-    TMEXEC=$(which tmux)
-    tmux_format="#{window_id}:#{?window_active_clients,a,n}:#{session_group}"
-    tmux list-windows -F $tmux_format |
+    tmux list-windows -F $TMUX_FORMAT |
         grep ":n:${TMGROUP}$" |
         while read -r window; do
             win_id=$(echo $window | cut -d: -f1)
@@ -31,26 +33,38 @@ function reattach-all() {
         done
 }
 
+function get-windows() {
+    tmux list-windows -F "$TMUX_FORMAT" | grep ":n:$TMGROUP$"
+}
 # If there are any windows with alacritty group and not attached to a client
 # then create a new alacritty window and prompt user to select a tmux window
 function init-select-window() {
-    tmux_format="#{window_id}:#{pane_current_command}:#W:#{?window_active_clients,a,n}:#{session_group}"
-    # Gets only windows with alacritty group and not attached to a client
-    windows=$(tmux list-windows -F "$tmux_format" | grep ":n:$TMGROUP$")
+    windows=$(get-windows)
     if [ ! -z "$windows" ]; then
-        alacritty msg create-window --hold -e /bin/zsh \
+        alacritty msg create-window -e /bin/zsh \
             -c "~/.config/alacritty/scripts.sh select-window '$windows'"
     fi
 }
 
+function format-windows-list() {
+    echo $1 | awk -F: '{printf "%-4s (%s) %s\n", $1, $2, $3}'
+}
+
 # Create a new alacritty window and attach to a tmux session selected by user
 function select-window() {
-    printf '\e]0;%s\e\\' "Activate persistent window"
+    printf '\e]0;%s\e\\' "Manage persistent shells"
     if [ -z "$1" ]; then
         echo "No windows provided"
         exit 1
     fi
-    sel_win=$(echo $1 | awk -F: '{printf "%-4s (%s) %s\n", $1, $2, $3}' | fzf --height 40% --sort --reverse)
+    sel_win=$(
+        format-windows-list $1 |
+            fzf --height 40% --sort --reverse \
+                --header=$'Manage persistent shells\nENTER: attach, CTRL-K: kill, CTRL-C: cancel' \
+                --header-first \
+                --bind 'enter:accept' \
+                --bind 'ctrl-k:execute('$CMD_KILL')+reload('$CMD_RELOAD')'
+    )
     sel_win_id=$(echo $sel_win | cut -w -f1)
     ses_name=$sel_win_id # tmux session name is the same as window id
     if [ ! -z "$sel_win_id" ]; then
